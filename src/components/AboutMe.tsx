c94c794131b4db13f24e27bccbf482863c540f14
Matthew Sweet <themattbook@gmail.com>
2023-12-30 03:14:06 -0600

    Updated UI for Main

--- src/components/AboutMe.tsx
+++ src/components/AboutMe.tsx
@@ -2,7 +2,7 @@ function AboutMe() {
     return (
         <div className="about-me height-screen">
             <div className="container height-full">
-                <div className="row height-full align-center">
+                <div className="row height-full align-items-center">
                     <div className="col-12-xs col-4-lg">
                         <h2>Experience</h2>
                     </div>
@@ -10,13 +10,13 @@ function AboutMe() {
                         <div className="row gap-2">
                             <div className="col-12-xs col-6-sm">
                                 <div className="work">
-                                    <p className="work-year">2022</p>
-                                    <p className="work-title">Company Name</p>
+                                    <p className="work-year">22-23</p>
+                                    <p className="work-title">DFN Freight</p>
                                     <p className="work-body">
                                         As a Frontend Developer, I was brought
                                         into the company as a contractor to
-                                        build a functional frontend SPA from
-                                        scratch, working with the company CTO in
+                                        build a functional SPA from scratch,
+                                        working with the company CTO in
                                         determining the technology and
                                         methodology used to carry a product to
                                         MVP.
@@ -30,61 +30,56 @@ function AboutMe() {
                             </div>
                             <div className="col-12-xs col-6-sm">
                                 <div className="work">
-                                    <p className="work-year">2022</p>
-                                    <p className="work-title">Company Name</p>
+                                    <p className="work-year">19-22</p>
+                                    <p className="work-title">OpenTeknik</p>
                                     <p className="work-body">
-                                        As a Frontend Developer, I was brought
-                                        into the company as a contractor to
-                                        build a functional frontend SPA from
-                                        scratch, working with the company CTO in
-                                        determining the technology and
-                                        methodology used to carry a product to
-                                        MVP.
+                                        As a Fullstack Developer, I worked with
+                                        the creators of Open Source Social
+                                        Network to improve their company website
+                                        as well as a social media framework
+                                        client for customers. Including but not
+                                        limited to R&amp;D with Javascript
+                                        Frameworks.
                                     </p>
                                     <p className="work-tech">
-                                        Vue 3, TypeScript, Pinia, Prisma,
-                                        NestJS, Jest, Cypress, Amazon Cognito,
-                                        Amazon EB, SonarCloud, and Bitbucket
+                                        React, HTML, CSS, PHP, MySQL, Nodejs
                                     </p>
                                 </div>
                             </div>
                             <div className="col-12-xs col-6-sm">
                                 <div className="work">
-                                    <p className="work-year">2022</p>
-                                    <p className="work-title">Company Name</p>
+                                    <p className="work-year">15-19</p>
+                                    <p className="work-title">GetBusy LLC.</p>
                                     <p className="work-body">
-                                        As a Frontend Developer, I was brought
-                                        into the company as a contractor to
-                                        build a functional frontend SPA from
-                                        scratch, working with the company CTO in
-                                        determining the technology and
-                                        methodology used to carry a product to
-                                        MVP.
+                                        Designed and co-developed a social media
+                                        automation platform using Open Source
+                                        Social Network, React, Node, Express,
+                                        Bootstrap, HTML/CSS, and more using
+                                        Agile methodology to take the product
+                                        from conception to production.
                                     </p>
                                     <p className="work-tech">
-                                        Vue 3, TypeScript, Pinia, Prisma,
-                                        NestJS, Jest, Cypress, Amazon Cognito,
-                                        Amazon EB, SonarCloud, and Bitbucket
+                                        React, HTML, CSS, PHP, MySQL, OSSN,
+                                        Express, Bootstrap, Nodejs
                                     </p>
                                 </div>
                             </div>
                             <div className="col-12-xs col-6-sm">
                                 <div className="work">
-                                    <p className="work-year">2022</p>
-                                    <p className="work-title">Company Name</p>
+                                    <p className="work-year">08-23</p>
+                                    <p className="work-title">Freelance</p>
                                     <p className="work-body">
-                                        As a Frontend Developer, I was brought
-                                        into the company as a contractor to
-                                        build a functional frontend SPA from
-                                        scratch, working with the company CTO in
-                                        determining the technology and
-                                        methodology used to carry a product to
-                                        MVP.
+                                        Spearheading the design and development
+                                        of diverse web projects leveraging HTML,
+                                        CSS, Javascript, Vue, and ReactJS for a
+                                        broad clientele; forging strong
+                                        collaborations to craft customized web
+                                        solutions driving business growth and
+                                        success.
                                     </p>
                                     <p className="work-tech">
-                                        Vue 3, TypeScript, Pinia, Prisma,
-                                        NestJS, Jest, Cypress, Amazon Cognito,
-                                        Amazon EB, SonarCloud, and Bitbucket
+                                        React, Vue, HTML, CSS, PHP, MySQL, Sass,
+                                        Bootstrap, Node, and more...
                                     </p>
                                 </div>
                             </div>
--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -14,84 +14,35 @@ function Main() {
         };
         updatePhrase();
 
-        const intervalId = setInterval(updatePhrase, 10000);
+        const intervalId = setInterval(updatePhrase, 5000);
         return () => clearInterval(intervalId);
     }, [phrases]);
     return (
         <div className="main height-screen">
-            <main className="container height-full">
-                <div className="row height-full align-center">
-                    <div className="col-1-xs col-5-lg height-full logo-container"></div>
-                    <div className="col-12-xs col-7-lg">
-                        <h2 className="mt-5">
+            <main className="row align-content-center container height-full">
+                <div className="row justify-center align-items-center">
+                    <div className="col-12-xs col-2-lg second">
+                        <div className="row-col align-content-center justify-center">
+                            <h1>
+                                <FaReact className="mr-3 icon" />
+                            </h1>
+                            <h1>
+                                <FaVuejs className="mr-3 icon" />
+                            </h1>
+                            <h1>
+                                <FaSass className="mr-3 icon" />
+                            </h1>
+                            <h1>
+                                <SiTailwindcss className="mr-3 icon" />
+                            </h1>
+                        </div>
+                    </div>
+                    <div className="col-12-xs col-8-lg">
+                        <h2>
                             I&apos;m Matt. I&apos;m a FullStack Developer who
                             loves UI/UX, Minimalism, and{" "}
                             <span className="phrase">{phrase}</span>
                         </h2>
-                        <div className="row justify-flex-end mt-5">
-                            <div className="col-12-xs col-12-lg">
-                                <div className="row gap-2 justify-flex-end">
-                                    <div className="col-12-xs col-6-sm">
-                                        <div className="card">
-                                            <p className="card-title">
-                                                <FaReact className="mr-3 icon" />{" "}
-                                                React
-                                            </p>
-                                            <p className="card-body">
-                                                My favorite framework. I have
-                                                extensive experience creating
-                                                React applications.
-                                            </p>
-                                        </div>
-                                    </div>
-                                    <div className="col-12-xs col-6-sm">
-                                        <div className="card">
-                                            <p className="card-title">
-                                                <FaVuejs className="mr-3 icon" />{" "}
-                                                Vue
-                                            </p>
-                                            <p className="card-body">
-                                                Nothing beats Vue 3, TypeScript,
-                                                and Pinia for building fast,
-                                                reactive applications.
-                                            </p>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                            <div className="col-12-xs col-12-lg">
-                                <div className="row gap-2 justify-flex-end">
-                                    <div className="col-12-xs col-6-sm">
-                                        <div className="card">
-                                            <p className="card-title">
-                                                <FaSass className="mr-3 icon" />{" "}
-                                                Sass
-                                            </p>
-                                            <p className="card-body">
-                                                I&apos;ve learned to embrace it
-                                                for it&apos;s flexibility and
-                                                elegance without inline selector
-                                                pollution.
-                                            </p>
-                                        </div>
-                                    </div>
-                                    <div className="col-12-xs col-6-sm">
-                                        <div className="card">
-                                            <p className="card-title">
-                                                <SiTailwindcss className="mr-3 icon" />{" "}
-                                                Tailwind
-                                            </p>
-                                            <p className="card-body">
-                                                I love it because it&apos;s easy
-                                                to work with. I have
-                                                professional experience building
-                                                SPA&apos;s with Tailwind.
-                                            </p>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
                     </div>
                 </div>
             </main>
